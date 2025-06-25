f565926626859a80aded0c33433e2da14ee16df0
Bhagavan11 <bhagavan9182@gmail.com>
2025-06-25 16:26:38 +0530

    Updated CORS and backend URLs for deployment55

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,11 +5,11 @@ export const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
   
     res.cookie("jwt-netflix", token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 15,
-      sameSite: "None",   // ✅ allow frontend from another domain
-      secure: true,       // ✅ required when using HTTPS (like Vercel + Render)
-    });
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 15,
+        sameSite: "None",     // 👈 Required for cross-site cookies
+        secure: true          // 👈 Required for HTTPS (like Render & Vercel)
+      });
   
     return token;
   };
